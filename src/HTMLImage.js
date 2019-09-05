80b4f3b0137e26b17922a438fa937078d818e65d
Dan Philibin <dan@danphilibin.com>
2019-09-04 19:35:23 -0700

    Only use styleWidth/styleHeight if the width is less than/equal to the maximum width

--- src/HTMLImage.js
+++ src/HTMLImage.js
@@ -80,10 +80,13 @@ export default class HTMLImage extends PureComponent {
         const { styleWidth, styleHeight } = this.getDimensionsFromStyle(style, height, width);
 
         if (styleWidth && styleHeight) {
-            return this.mounted && this.setState({
-                width: typeof styleWidth === 'string' && styleWidth.search('%') !== -1 ? styleWidth : parseInt(styleWidth, 10),
-                height: typeof styleHeight === 'string' && styleHeight.search('%') !== -1 ? styleHeight : parseInt(styleHeight, 10)
-            });
+            // Only use styleWidth/styleHeight if the width is less than/equal to the maximum width
+            if (!imagesMaxWidth || styleWidth <= imagesMaxWidth) {
+                return this.mounted && this.setState({
+                    width: typeof styleWidth === 'string' && styleWidth.search('%') !== -1 ? styleWidth : parseInt(styleWidth, 10),
+                    height: typeof styleHeight === 'string' && styleHeight.search('%') !== -1 ? styleHeight : parseInt(styleHeight, 10)
+                });
+            }
         }
         // Fetch image dimensions only if they aren't supplied or if with or height is missing
         Image.getSize(
